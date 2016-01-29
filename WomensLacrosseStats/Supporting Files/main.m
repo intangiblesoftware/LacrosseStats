ebded9d433c30cf1ef9f8131c69f56736a6df1e0
Jim Dabrowski <jimdab@mac.com>
2016-01-29 07:24:38 -0600

    Added Women's Stats Target
    Added Women’s Stats target to project to create women’s app virtually
    the same as the men’s app. Compiles, runs. yay.

---
+++ WomensLacrosseStats/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WomensLacrosseStats
+//
+//  Created by James Dabrowski on 1/28/16.
+//  Copyright © 2016 Intangible Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WomensAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WomensAppDelegate class]));
+    }
+}
