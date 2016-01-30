00cf5f0cf00ff32a2fb7a7d82d6a236d14fb04f8
Jim Dabrowski <jimdab@mac.com>
2016-01-29 20:31:36 -0600

    Updated women's part to display a little better. Started tweaking the stats that are recorded for their games.

--- MensLacrosseStats/View Controllers/CollectionViewCells/INSOPlayerCollectionViewCell.m
+++ MensLacrosseStats/View Controllers/CollectionViewCells/INSOPlayerCollectionViewCell.m
@@ -19,13 +19,13 @@
     UIView *bgView = [[UIView alloc]initWithFrame:self.bounds];
     self.backgroundView = bgView;
     self.backgroundView.backgroundColor = [UIColor whiteColor];
-    self.backgroundView.layer.borderColor = [[UIColor scorebookBlue] CGColor];
+    self.backgroundView.layer.borderColor = [self.highlightColor CGColor];
     self.backgroundView.layer.borderWidth = 1.0;
     
     // selected background
     UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
     self.selectedBackgroundView = selectedView;
-    self.selectedBackgroundView.backgroundColor = [UIColor scorebookBlue];
+    self.selectedBackgroundView.backgroundColor = self.highlightColor;
     self.selectedBackgroundView.layer.borderColor = [[UIColor whiteColor] CGColor];
     self.selectedBackgroundView.layer.borderWidth = 1.0;
 }
