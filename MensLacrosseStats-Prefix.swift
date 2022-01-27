b9df88b7148d88f5986225947b581fd282e48986
Jim <jimdab@mac.com>
2022-01-27 10:11:31 -0700

    First stage of swiftifying

---
+++ MensLacrosseStats-Prefix.swift
@@ -0,0 +1,4 @@
+//  Converted to Swift 5.5 by Swiftify v5.5.27463 - https://swiftify.com/
+//
+// Prefix header for all source files of the `MensLacrosseStats` target in the 'LacrosseStats' project.
+//
